649018904a9b7ef15dda136709eb7f97e2e390e4
Nikhitha Talluri <nikhitha@Nikhithas-MacBook-Pro.local>
2023-05-01 14:40:35 +0530

    css changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,60 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import "../styles/nav.css"
+import React, { useState } from 'react';
+import '../styles/nav.css';
+
 function Navbar() {
-	
+  const [active, setActive] = useState("nav__menu");
+  const [icon, setIcon] = useState("nav__toggler");
+  const navToggle = () => {
+    if (active === "nav__menu") {
+      setActive("nav__menu nav__active");
+    } else setActive("nav__menu");
+
+    // Icon Toggler
+    if (icon === "nav__toggler") {
+      setIcon("nav__toggler toggle");
+    } else setIcon("nav__toggler");
+  };
   return (
-	<nav>
-	<ul className="list">
-	<Link to="/" className='items'>Home</Link>
-	<Link to="/diabetes" className='items'>Diabetes</Link>
-	<Link to="/liver" className='items'>Liver</Link>
-	<Link to="/kidney" className='items'>Kidney</Link>
-	<Link to="/heart" className='items'>Heart</Link>
-	<Link to="/cancer" className='items'>Cancer</Link>
-	</ul>
-	<button className="btn">BTN</button>
+    <nav className="nav">
+      <ul className={active}>
+        <li className="nav__item">
+          <a href="/" className="nav__link">
+            Home
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="/diabetes" className="nav__link">
+           Diabetes
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="/liver" className="nav__link">
+            Liver Disease
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="/kidney" className="nav__link">
+            Kidney Disease
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="/heart" className="nav__link">
+            Heart Disease
+          </a>
+        </li>
+		<li className="nav__item">
+          <a href="/cancer" className="nav__link">
+            Breast Cancer
+          </a>
+        </li>
+      </ul>
+      <div onClick={navToggle} className={icon}>
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
+      </div>
     </nav>
-  )
+  );
 }
-export default Navbar
+
+export default Navbar;
